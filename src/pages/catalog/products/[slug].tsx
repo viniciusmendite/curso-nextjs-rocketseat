46043d2e7663aa2450ce255442e054d75e14dba6
Vinicius Mendite <vinicius.miff@gmail.com>
2020-12-23 12:39:42 -0300

    lazy loading

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,8 +1,29 @@
 import {useRouter} from 'next/router'
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Loading...</p>, ssr: false } // ssr: false, diz que vai ser redenrizado somente no lado do cliente, pq assim conseguimos usar as variaveis globais presentes no browser, como document, window etc
+) 
 
 export default function Product(){
 
 const router = useRouter()
+const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-return <h1>{router.query.slug}</h1>
+
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      { isAddToCartModalVisible && <AddToCartModal /> }
+    </div>
+  );
 }
